aefa8a6d4e7edb32e1bfd8450592e444ec2be3f1
Pankaj Kumar <104948046+PankajKumardev@users.noreply.github.com>
2024-05-29 12:43:50 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,45 @@
+const brailleMap = {
+    "A": "⠁", "B": "⠃", "C": "⠉", "D": "⠙", "E": "⠑", "F": "⠋",
+    "G": "⠛", "H": "⠓", "I": "⠊", "J": "⠚", "K": "⠅", "L": "⠇",
+    "M": "🇲", "N": "⠝", "O": "⠕", "P": "⠏", "Q": "⠟", "R": "⠗",
+    "S": "⠎", "T": "⠞", "U": "ユー", "V": "⠧", "W": "ᚹ", "X": "⠭",
+    "Y": "យ", "Z": "⠺"
+  };
+  const small = document.getElementById("small");
+  const medium = document.getElementById("medium");
+  const big = document.getElementById("Big");
+  const braille = document.getElementById("brailleOutput");
+
+  function convertToBraille() {
+    const inputText = document.getElementById("inputText").value.toUpperCase();
+    let brailleOutput = "";
+    for (let char of inputText) {
+        if (brailleMap[char]) {
+          brailleOutput += brailleMap[char];
+        } else {
+          brailleOutput += " "; 
+        }
+      }
+      document.getElementById("brailleOutput").textContent = brailleOutput;
+      braille.style.fontSize = "50px";
+      braille.style.width = "100px";
+        braille.style.height = "300px";
+    }  
+
+    small.addEventListener("click",()=>{
+        braille.style.fontSize = "50px";
+        braille.style.width = "100px";
+        braille.style.height = "200px";
+    })
+
+    medium.addEventListener("click",()=>{
+        braille.style.fontSize = "100px";
+        braille.style.width = "100px";
+        braille.style.height = "200px";
+    })
+
+    big.addEventListener("click",()=>{
+        braille.style.fontSize = "150px";
+        braille.style.width = "100px";
+        braille.style.height = "200px";
+    })
